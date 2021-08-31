03ee578a5626b7c518e52be8b7afbb8226ad0d85
Kenny Corman <kennywcorman@gmail.com>
2021-08-31 20:00:01 +0100

    Sound handler

--- sound_handler.py
+++ sound_handler.py
@@ -10,6 +10,7 @@ import logging
 import random
 import simple_sound_lib
 
+logging.basicConfig(level=logging.DEBUG)
 class Sound:
     def __init__(self, dirname):
         # name of directory that the sounds reside in
@@ -21,6 +22,8 @@ class Sound:
         self.files = [f for f in listdir(self.dirname) if isfile(join(self.dirname, f))]
         if(len(self.files) < 1):
             logging.warn("sound with dir " + self.dirname + " has no files found in the directory")
+        else:
+            logging.debug("sound with dir " + self.dirname + " found files " + str(self.files))
         self.file_index = random.randrange(len(self.files))
 
     def num_files(self):
@@ -35,23 +38,27 @@ class Sound:
     def is_playing(self):
         if(self.curr_sound_handle == None):
             return False
-        return not is_finished(self.curr_sound_handle)
+        return not simple_sound_lib.is_finished(self.curr_sound_handle)
 
     def play_next_sound(self):
         if(self.is_playing()):
             logging.warn("play_next_sound invoked for sound " + self.dirname + " but sound is already in progress")
         else:
             f = self.next_file()
-            self.curr_sound_handle = play_sound(f)
+            self.curr_sound_handle = simple_sound_lib.play_sound(join(self.dirname, f))
 
     def stop_current_sound(self):
         if(self.curr_sound_handle != None):
-            stop_sound(self.curr_sound_handle)
+            simple_sound_lib.stop_sound(self.curr_sound_handle)
 
-snd = Sound("./sounds/tmp")
+snd = Sound("./sounds/tmp/")
 while(True):
     snd.play_next_sound()
+    itr=0
     while(snd.is_playing()):
         print("sound still playing, waiting...")
         time.sleep(1)
+        itr+=1
+        if(itr > 6):
+            snd.stop_current_sound()
     print("sound done")
---
+++ sounds/get_youtube_sound.sh
@@ -0,0 +1,1 @@
+youtube-dl -x --audio-format mp3 --output "%(uploader)s%(title)s.%(ext)s" $1
