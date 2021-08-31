6b9ebb7eaac07730f245a6752d6f7fa120d3dd66
Kenny <kennywcorman@gmail.com>
2021-08-31 11:46:33 -0700

    Started sound handler

---
+++ sound_handler.py
@@ -0,0 +1,57 @@
+# Defines the main sound handling logic
+# this is loosely coupled to the gpio handling class because it
+# also needs to set "virtual sound channels" appropriately
+# The core idea is that the caller can play a sound on a virtual channel by name
+# the caller can also stop sounds by name
+from os import listdir
+from os.path import isfile, join
+import time
+import logging
+import random
+import simple_sound_lib
+
+class Sound:
+    def __init__(self, dirname):
+        # name of directory that the sounds reside in
+        self.dirname = dirname
+        self.update_known_files()
+        self.curr_sound_handle = None
+
+    def update_known_files(self):
+        self.files = [f for f in listdir(self.dirname) if isfile(join(self.dirname, f))]
+        if(len(self.files) < 1):
+            logging.warn("sound with dir " + self.dirname + " has no files found in the directory")
+        self.file_index = random.randrange(len(self.files))
+
+    def num_files(self):
+        return len(self.files)
+
+    def next_file(self):
+        if(self.num_files() < 1):
+            return None
+        self.file_index = (self.file_index + 1) % self.num_files()
+        return self.files[self.file_index] 
+
+    def is_playing(self):
+        if(self.curr_sound_handle == None):
+            return False
+        return not is_finished(self.curr_sound_handle)
+
+    def play_next_sound(self):
+        if(self.is_playing()):
+            logging.warn("play_next_sound invoked for sound " + self.dirname + " but sound is already in progress")
+        else:
+            f = self.next_file()
+            self.curr_sound_handle = play_sound(f)
+
+    def stop_current_sound(self):
+        if(self.curr_sound_handle != None):
+            stop_sound(self.curr_sound_handle)
+
+snd = Sound("./sounds/tmp")
+while(True):
+    snd.play_next_sound()
+    while(snd.is_playing()):
+        print("sound still playing, waiting...")
+        time.sleep(1)
+    print("sound done")
