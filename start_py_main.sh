12c801780b110db23c300a5b344c2a4b2fa096bb
Kenny Corman <kennywcorman@gmail.com>
2021-09-02 01:08:09 +0100

    Turn off logging for now

--- start_py_main.sh
+++ start_py_main.sh
@@ -1,5 +1,7 @@
 logsuffix=`date +%s`
 new_log_name="logs/app-${logsuffix}.log"
+#LOG_FILE=logs/app.log
+LOG_FILE=/dev/null
 echo "Rotating log file app.log to ${new_log_name}"
 mv logs/app.log "${new_log_name}"
-python3 main.py > logs/app.log 2>&1
+python3 main.py > $LOG_FILE 2>&1
